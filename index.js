cc8a8852ece5ebe778e4f30dd171d85ffb22f395
MusaMalikx <muhammadmusamalik2@gmail.com>
2022-12-23 20:06:30 +0500

    vercel configuration added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express, { application } from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import userRoutes from "./routes/users.js"
+import userRoutes from "./routes/users.js";
 import authRoutes from "./routes/auth.js";
 import artworkRoutes from "./routes/artworks.js";
 import cookieParser from "cookie-parser";
@@ -13,8 +13,8 @@ const MONGO = "mongodb://localhost:27017/Artsy";
 const connect = () => {
   mongoose.set("strictQuery", false);
   mongoose
-    .connect(MONGO , {
-      useNewUrlParser: true
+    .connect(MONGO, {
+      useNewUrlParser: true,
     })
     .then(() => {
       console.log("Connected to DB");
@@ -30,7 +30,6 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/artworks", artworkRoutes);
 
-
 //error handler
 app.use((err, req, res, next) => {
   const status = err.status || 500;
@@ -42,7 +41,10 @@ app.use((err, req, res, next) => {
   });
 });
 
-app.listen(5000 , () => {
-    connect();
-    console.log("Connected on 5000 port !");
-})
+app.listen(8080, () => {
+  connect();
+  console.log("Connected on 8080 port!");
+});
+
+// Export the Express API
+export default app;
