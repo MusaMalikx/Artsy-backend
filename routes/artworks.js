d53ee5924914b333aca8c7e851817eb6b11d1585
Abdullah Tahir <abd.tahir1122@gmail.com>
2023-01-16 20:06:27 +0500

    Multer Upload function seprated in utils file

--- routes/artworks.js
+++ routes/artworks.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const fs = require("fs");
 const { verifyToken } = require("../utils/verifyToken.js");
 const {
   add,
@@ -7,30 +6,7 @@ const {
   getallartworks,
   getartworkimage,
 } = require("../controllers/artwork.js");
-const multer = require("multer");
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    const dir = "./uploads/artworks/";
-    if (!fs.existsSync(dir)) {
-      fs.mkdirSync(dir);
-    }
-    cb(null, dir);
-  },
-  filename: function (req, file, cb) {
-    cb(null, new Date().toISOString().replace(/:/g, "-") + file.originalname);
-  },
-});
-const fileFilter = (req, file, cb) => {
-  if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
-    cb(null, true);
-  } else {
-    cb(null, false);
-  }
-};
-const upload = multer({
-  storage: storage,
-  fileFilter: fileFilter,
-});
+const { upload } = require("../utils/artworksUpload.js");
 const router = express.Router();
 
 //Create Artwork
